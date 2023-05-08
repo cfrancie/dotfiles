7e9571da798198b1ef069a1f5b76986bbd3a737a
cfrancie <celeste.francier@protonmail.com>
2023-05-08 23:55:41 +0200

    fish config

---
+++ config.fish
@@ -0,0 +1,48 @@
+set -x DEBUGINFOD_URLS "https://debuginfod.archlinux.org"
+## Set values
+# Hide welcome message
+set fish_greeting
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
+## Useful aliases
+# Replace ls with exa
+alias l='exa -al --color=always --group-directories-first --icons' # preferred listing
+alias la='exa -a --color=always --group-directories-first --icons'  # all files and dirs
+alias ll='exa -l --color=always --group-directories-first --icons'  # long format
+alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
+alias l.="exa -a | egrep '^\.'"                                     # show only dotfiles
+alias lr="exa --long --tree --level=2" 				    # tree format
+# Replace some more things with better alternatives
+alias cat='bat --style header --style rules --style snip --style changes --style header'
+[ ! -x /usr/bin/yay ] && [ -x /usr/bin/paru ] && alias yay='paru --bottomup'
+
+# Common use
+alias aup="pamac upgrade --aur"
+alias grubup="sudo update-grub"
+alias fixpacman="sudo rm /var/lib/pacman/db.lck"
+alias tarnow='tar -acf '
+alias untar='tar -zxvf '
+alias wget='wget -c '
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias n='nvim -O'
+alias c='clear'
+alias gcl='git clone'
+alias rf='rm -rf'
+alias work='cd /home/adl/Bureau/Codes/42-cursus'
+alias val='valgrind --leak-check=full --show-leak-kinds=all'
+alias cop='xclip -selection clipboard'
+
+# Cleanup orphaned packages
+alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
+
+# Get the error messages from journalctl
+alias jctl="journalctl -p 3 -xb"
